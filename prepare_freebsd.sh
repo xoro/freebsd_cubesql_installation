6470cab722caea23d59a315b2a39b9b408205bec
Timo Pallach <timo@pallach.de>
2018-07-10 15:34:36 +0200

    Added the linsysfs to /etc/fstab.

--- prepare_freebsd.sh
+++ prepare_freebsd.sh
@@ -4,12 +4,15 @@
 sysrc linux_enable="YES"
 service abi start
 pkg install linux-c7
-echo 'linprocfs /compat/linux/proc linprocfs rw 0 0' >> /etc/fstab
-echo 'tmpfs /compat/linux/dev/shm tmpfs rw,mode=1777 0 0' >> /etc/fstab
+echo 'linprocfs   /compat/linux/proc    linprocfs       rw           0       0' >> /etc/fstab
+echo 'linsysfs    /compat/linux/sys     linsysfs        rw           0       0' >> /etc/fstab
+echo 'tmpfs       /compat/linux/dev/shm tmpfs           rw,mode=1777 0       0' >> /etc/fstab
+mount /compat/linux/proc
+mount /compat/linux/sys
+mount /compat/linux/dev/shm
 cd /compat/linux/usr/lib
 ln -s libssl.so.10 libssl.so
 ln -s libcrypto.so.10 libcrypto.so
 cd /compat/linux/usr/lib64
 ln -s libssl.so.10 libssl.so
 ln -s libcrypto.so.10 libcrypto.so
-reboot
